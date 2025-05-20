c32188661d7324b5ca47fa8cc095f6ca811028aa
bhawani <22f2000273@ds.study.iitm.ac.in>
2025-05-21 00:44:12 +0530

    Your message describing the changes

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+import json
+app = FastAPI()
+with open("data.json") as f:
+    students = json.load(f)
+
+@app.get("/students")
+def get_students():
+    return students
